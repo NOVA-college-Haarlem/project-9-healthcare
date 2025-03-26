7674cca394e618e27147d4e712c36eacf16a1b07
Mona Lisa <mona@Lisa.com>
2025-03-26 13:49:03 +0100

    seeders fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,41 +25,12 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            PatientSeeder::class,
+            DepartmentSeeder::class,
+            VaccinationSeeder::class,
             ScheduleSeeder::class,
+            DoctorSeeder::class,
         ]);
-    }
-}
 
-        // Eerst departments aanmaken
-        
-
-        // Artsen aanmaken
-        
-
-        // Patiënten met vaccinaties
-
-        // Aanvullende testpatiënten zonder vaccinaties
-        for ($i = 1; $i <= 10; $i++) {
-            $user = User::firstOrCreate(
-                ['email' => 'patient'.$i.'@example.com'],
-                [
-                    'name' => 'Testpatiënt '.$i,
-                    'password' => Hash::make('password'),
-                    'email_verified_at' => now()
-                ]
-            );
-
-            Patient::firstOrCreate(
-                ['user_id' => $user->id],
-                [
-                    'date_of_birth' => Carbon::now()->subYears(rand(5, 70))->subMonths(rand(0, 11))->subDays(rand(0, 30)),
-                    'gender' => ['male', 'female', 'other'][rand(0, 2)],
-                    'address' => 'Voorbeeldstraat '.$i.', 123'.$i.' AB Teststad',
-                    'phone' => '06'.rand(10000000, 99999999),
-                    'emergency_contact' => '06'.rand(10000000, 99999999).' (Noodcontact)',
-                    'blood_type' => ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'][rand(0, 7)]
-                ]
-            );
-        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,27 +32,22 @@ Route::name("schedules.")->group(function(){
     });
 });
 
-
-require __DIR__.'/auth.php';
-
-
 # VACCINATIONS ROUTES
 Route::prefix('vaccinations')->group(function () {
     // Reminders
-    Route::get('/reminders', [VaccinationController::class, 'reminders'])->name('vaccinations.reminders');
-    Route::get('/', [VaccinationController::class, 'index'])->name('vaccinations.index');
-    Route::get('/create', [VaccinationController::class, 'create'])->name('vaccinations.create');
-    Route::post('/', [VaccinationController::class, 'store'])->name('vaccinations.store');
-    Route::get('/{vaccination}', [VaccinationController::class, 'show'])->name('vaccinations.show');
-    Route::get('/{vaccination}/edit', [VaccinationController::class, 'edit'])->name('vaccinations.edit');
-    Route::put('/{vaccination}', [VaccinationController::class, 'update'])->name('vaccinations.update');
-    Route::delete('/{vaccination}', [VaccinationController::class, 'destroy'])->name('vaccinations.destroy');
-    
+    Route::get('/reminders',            [VaccinationController::class, 'reminders'])->name('vaccinations.reminders');
+    Route::get('/',                     [VaccinationController::class, 'index'])->name('vaccinations.index');
+    Route::get('/create',               [VaccinationController::class, 'create'])->name('vaccinations.create');
+    Route::post('/',                    [VaccinationController::class, 'store'])->name('vaccinations.store');
+    Route::get('/{vaccination}',        [VaccinationController::class, 'show'])->name('vaccinations.show');
+    Route::get('/{vaccination}/edit',   [VaccinationController::class, 'edit'])->name('vaccinations.edit');
+    Route::put('/{vaccination}',        [VaccinationController::class, 'update'])->name('vaccinations.update');
+    Route::delete('/{vaccination}',     [VaccinationController::class, 'destroy'])->name('vaccinations.destroy');
+
     // Patient-specific routes
-    Route::get('/patient/{patient}', [VaccinationController::class, 'patientHistory'])->name('vaccinations.patient.history');
-    Route::get('/patient/{patient}/schedule', [VaccinationController::class, 'upcomingVaccines'])->name('vaccinations.patient.schedule');
+    Route::get('/patient/{patient}',             [VaccinationController::class, 'patientHistory'])->name('vaccinations.patient.history');
+    Route::get('/patient/{patient}/schedule',    [VaccinationController::class, 'upcomingVaccines'])->name('vaccinations.patient.schedule');
     Route::get('/patient/{patient}/certificate', [VaccinationController::class, 'showCertificate'])->name('vaccinations.patient.certificate');
-    
 });
 
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
